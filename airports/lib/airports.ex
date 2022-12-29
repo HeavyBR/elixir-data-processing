73d1beab16fb4cf8b0b4435b6ee0efedb361d917
Matheus Cumpian <matheus.cumpian@hotmail.com>
2022-12-29 00:24:36 -0300

    :alembic: Adding Flow to process data stream from CSV

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,18 +1,27 @@
 defmodule Airports do
-  @moduledoc """
-  Documentation for `Airports`.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
 
-  @doc """
-  Hello world.
+  def airpots_csv(), do: Application.app_dir(:airports, "/priv/airports.csv")
+  def open_airports() do
+    airpots_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(&(&1.country))
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Enum.to_list()
+    |> List.flatten()
 
-  ## Examples
-
-      iex> Airports.hello()
-      :world
-
-  """
-  def hello do
-    :world
   end
 end
--- airports/test/airports_test.exs
+++ airports/test/airports_test.exs
@@ -2,7 +2,5 @@ defmodule AirportsTest do
   use ExUnit.Case
   doctest Airports
 
-  test "greets the world" do
-    assert Airports.hello() == :world
-  end
+
 end
